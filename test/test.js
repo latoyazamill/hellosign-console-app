e6429d6869222a45d693d168e2fd89f116547e70
LaToya Williams <latoyazamill@yahoo.com>
2018-10-23 09:12:34 -0700

    Create a test for signatureRequestRemind

--- test/test.js
+++ test/test.js
@@ -3,30 +3,30 @@ const request = require('../requests/request');
 const config = require('../config.js');
 
 describe('Request', function () {
-  // it('sendSignature', function () {
-  //   return request.sendSignatureRequest()
-  //     .then(function (results) {
-  //       expect(results.statusMessage).to.equal('OK');
-  //     })
-  // });
-  // it('signatureRequestGet', function () {
-  //   return request.signatureRequestGet()
-  //     .then(function (results) {
-  //       expect(results.signature_request.signature_request_id).to.equal(config.SIGREQ);
-  //     })
-  // });
-  // it('sendSignatureRequestWithTemplate', function () {
-  //   return request.sendSignatureRequestWithTemplate()
-  //     .then(function (results) {
-  //       expect(results.signature_request.title).to.equal('Purchase Order');
-  //     })
-  // });
-  // it('sendSignatureRequestWithTemplateCf', function () {
-  //   return request.sendSignatureRequestWithTemplateCf()
-  //     .then(function (results) {
-  //       expect(results.signature_request.custom_fields).to.be.an('array');
-  //     })
-  // });
+  it('sendSignature', function () {
+    return request.sendSignatureRequest()
+      .then(function (results) {
+        expect(results.statusMessage).to.equal('OK');
+      })
+  });
+  it('signatureRequestGet', function () {
+    return request.signatureRequestGet()
+      .then(function (results) {
+        expect(results.signature_request.signature_request_id).to.equal(config.SIGREQ);
+      })
+  });
+  it('sendSignatureRequestWithTemplate', function () {
+    return request.sendSignatureRequestWithTemplate()
+      .then(function (results) {
+        expect(results.signature_request.title).to.equal('Purchase Order');
+      })
+  });
+  it('sendSignatureRequestWithTemplateCf', function () {
+    return request.sendSignatureRequestWithTemplateCf()
+      .then(function (results) {
+        expect(results.signature_request.custom_fields).to.be.an('array');
+      })
+  });
   it('signatureRequestRemind', function () {
     return request.signatureRequestRemind()
       .then(function (results) {
