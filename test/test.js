9f674c37be5714736432b1b8be8d7a1e392d1f41
LaToya Williams <latoyazamill@yahoo.com>
2018-10-23 09:57:12 -0700

    Create a test for createEmbeddedUncliamedDraftErWs

--- test/test.js
+++ test/test.js
@@ -3,62 +3,68 @@ const request = require('../requests/request');
 const config = require('../config.js');
 
 describe('Request', function () {
-  it('sendSignature', function () {
-    return request.sendSignatureRequest()
-      .then(function (results) {
-        expect(results.statusMessage).to.equal('OK');
-      })
-  });
-  it('signatureRequestGet', function () {
-    return request.signatureRequestGet()
-      .then(function (results) {
-        expect(results.signature_request.signature_request_id).to.equal(config.SIGREQ);
-      })
-  });
-  it('sendSignatureRequestWithTemplate', function () {
-    return request.sendSignatureRequestWithTemplate()
-      .then(function (results) {
-        expect(results.signature_request.title).to.equal('Purchase Order');
-      })
-  });
-  it('sendSignatureRequestWithTemplateCf', function () {
-    return request.sendSignatureRequestWithTemplateCf()
-      .then(function (results) {
-        expect(results.signature_request.custom_fields).to.be.an('array');
-      })
-  });
-  it('signatureRequestRemind', function () {
-    return request.signatureRequestRemind()
-      .then(function (results) {
-        expect(results.signature_request_id).to.equal(config.SIGREQ);
-      })
-  });
-  it('accountGet', function () {
-    return request.accountGet()
-      .then(function (results) {
-        expect(results.account.account_id).to.equal(config.ACCOUNTID);
-      })
-  });
-  it('templateGet', function () {
-    return request.templateGet()
-      .then(function (results) {
-        expect(results.template.template_id).to.equal(config.TEMPID);
-      })
-  });
-  it('createEmbedded', function () {
-    return request.createEmbedded()
-      .then(function (results) {
-        expect(results.embedded.sign_url).to.be.a('string');
-      })
-  });
+  // it('sendSignature', function () {
+  //   return request.sendSignatureRequest()
+  //     .then(function (results) {
+  //       expect(results.statusMessage).to.equal('OK');
+  //     })
+  // });
+  // it('signatureRequestGet', function () {
+  //   return request.signatureRequestGet()
+  //     .then(function (results) {
+  //       expect(results.signature_request.signature_request_id).to.equal(config.SIGREQ);
+  //     })
+  // });
+  // it('sendSignatureRequestWithTemplate', function () {
+  //   return request.sendSignatureRequestWithTemplate()
+  //     .then(function (results) {
+  //       expect(results.signature_request.title).to.equal('Purchase Order');
+  //     })
+  // });
+  // it('sendSignatureRequestWithTemplateCf', function () {
+  //   return request.sendSignatureRequestWithTemplateCf()
+  //     .then(function (results) {
+  //       expect(results.signature_request.custom_fields).to.be.an('array');
+  //     })
+  // });
+  // it('signatureRequestRemind', function () {
+  //   return request.signatureRequestRemind()
+  //     .then(function (results) {
+  //       expect(results.signature_request_id).to.equal(config.SIGREQ);
+  //     })
+  // });
+  // it('accountGet', function () {
+  //   return request.accountGet()
+  //     .then(function (results) {
+  //       expect(results.account.account_id).to.equal(config.ACCOUNTID);
+  //     })
+  // });
+  // it('templateGet', function () {
+  //   return request.templateGet()
+  //     .then(function (results) {
+  //       expect(results.template.template_id).to.equal(config.TEMPID);
+  //     })
+  // });
+  // it('createEmbedded', function () {
+  //   return request.createEmbedded()
+  //     .then(function (results) {
+  //       expect(results.embedded.sign_url).to.be.a('string');
+  //     })
+  // });
   // it('createEmbeddedWithTemplate', function () {
   //   return request.createEmbeddedWithTemplate()
   //     .then(function (results) {
   //       expect(results.embedded.sign_url).to.be.a('string');
   //     })
   // });
-  it('createEmbeddedUnclaimedDraftEr', function () {
-    return request.createEmbeddedUnclaimedDraftEr()
+  // it('createEmbeddedUnclaimedDraftEr', function () {
+  //   return request.createEmbeddedUnclaimedDraftEr()
+  //     .then(function (results) {
+  //       expect(results.unclaimed_draft.claim_url).to.be.a('string');
+  //     })
+  // });
+  it('createEmbeddedUnclaimedDraftErWs', function () {
+    return request.createEmbeddedUnclaimedDraftErWs()
       .then(function (results) {
         expect(results.unclaimed_draft.claim_url).to.be.a('string');
       })
